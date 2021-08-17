c47ef7afc58743386db8d3ae8dc5ff2093185294
DavidHGJ <davidhcc.2015@gmail.com>
2021-08-17 19:38:57 -0300

    Update - Composer adicionado (Autoload) - 17/08

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '60fdc0d5d3c9a0bca23538d94f5d67d7be52cc26',
+        'name' => 'pineapplestorm/pineapplestorm',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pineapplestorm/pineapplestorm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '60fdc0d5d3c9a0bca23538d94f5d67d7be52cc26',
+            'dev_requirement' => false,
+        ),
+    ),
+);
