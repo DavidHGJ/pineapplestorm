63ea9cd4c5539d6fd545db624d226c526389f1b9
DavidHGJ <davidhcc.2015@gmail.com>
2021-08-18 20:17:49 -0300

    Update class Rota - 18/08

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-use app\models\class\teste;
-
 require __DIR__ . "/vendor/autoload.php";
 
-$url_atual = $_REQUEST['url'];
+$urlAtual = explode('/', $_REQUEST['url']);
 
-if (file_exists(PATH_PUBLIC . $url_atual . '.php'))
-    require_once PATH_PUBLIC . $url_atual . '.php';
-else echo 'Página não existe';
+$paginaInicial = 'login';
+
+// if (file_exists(PATH_PUBLIC . $urlAtual . '.php'))
+//     require_once PATH_PUBLIC . $urlAtual . '.php';
+// else
+//     require_once PATH_PUBLIC . $paginaInicial . '.php';
