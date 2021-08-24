e275a444c81f189bcda7d7f966dec97659eccb7f
Vinicius Becker <vinicius_2010_becker@hotmail.com>
2021-08-23 22:07:36 -0300

    update

--- public/pages/erro.php
+++ public/pages/erro.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    
     <?php
+        use app\helpers\inclusao;
+
         require_once PATH_INCLUDES . 'head.php';
+        inclusao::css(['erro'])
     ?>
 </head>
 <body>
-    <main>
-        <div>
-            <p>URL não encontrada, retorne para a página inícial</p>
-            <a href="<?= URL . 'login' ?>">Retornar</a>
-        </div>    
+    <main class="erro">
+       <header>
+           <h1>ERRO 404</h1>
+                <div >
+                    <img src="<?= URL_ASSETS . 'img/erro/erro.png' ?>" alt = "erro" >
+                    <p>URL não encontrada, retorne para a página inícial</p>
+                    <a href="<?= URL . 'login' ?>">Retornar</a>
+                </div> 
+        </header>       
     </main>
 </html>
 </body>
