818b5548c9386569ad98392b3f842a7a2cff70e1
DavidHGJ <davidhcc.2015@gmail.com>
2021-08-25 19:48:35 -0300

    Update - Helper Class Filtro - 25/08

--- app/helpers/filtro.php
+++ app/helpers/filtro.php
@@ -3,5 +3,16 @@
 namespace app\helpers;
 
 class Filtro {
-    
+
+    public static function request($nomeRequest) {
+
+        $request = $_REQUEST[$nomeRequest];
+
+        if (is_array($request)) {
+            foreach ($request as $dado) $requestTratado[] = trim(str_replace(['\\', '\'', '"', ',' , ';', '<', '>'], '', strval($dado)));
+            return $requestTratado;
+        }
+        else
+            return trim(str_replace(['\\', '\'', '"', ',' , ';', '<', '>'], '', strval($request)));
+    }
 }
