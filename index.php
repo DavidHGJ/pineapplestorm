57bd5418479c4fd57552a0a2c711d9e8f8735c17
DavidHGJ <davidhcc.2015@gmail.com>
2021-09-15 19:05:23 -0300

    Update

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php
 
-use models\class\queryManager\QueryManager;
-<<<<<<< HEAD
-=======
-use models\class\queryManager\Tabela;
-use models\class\queryManager\TableManager;
->>>>>>> 75684545782bd993b480f41dee342759386dc105
 use models\class\util\Rota;
-use models\class\queryManager\TableManager;
 
 require 'app/models/autoload.php';
 require 'vendor/autoload.php';
-
-//$tableManager = TableManager::getInstance();
-//$queryManager = QueryManager::getInstance()-> setAcao("SELECT")-> setTabela($tableManager->getTabela("usuario"));
 
 if ($_REQUEST['url'] != null)
     $urlAtual = explode('/', $_REQUEST['url']);
