1a5a2001ea6bf54795313afe337840e02062d6fb
Igor Cavichiolle <igorgabrielcavichiolle@hotmail.com>
2021-11-06 14:08:32 -0300

    06/11/2021 - Ligando CRUDS com o BackEnd

--- app/config/global.php
+++ app/config/global.php
@@ -7,6 +7,13 @@ define('METHOD', $_SERVER['REQUEST_METHOD']);
 define('PATH_APP', __DIR__ . '\\..\\..\\app\\');
 define('PATH_ROOT', __DIR__ . '\\..\\..\\');
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000');
+header('Content-Length: 0');
+header('Content-Type: text/plain');
+
 require_once PATH_APP . 'helpers/Filtro.php';
 
 // $urlRecuperada = explode('/', $_SERVER['REQUEST_URI']);
